f2ad43f4df58fd65be43edee66e7be18030c4a9e
Your New Username <Your Email Address Associated with Your GitHub Account>
2023-03-06 00:52:53 +0600

    first commit

---
+++ src/pickers/PayerPicker.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Autocomplete, useModulesManager, useTranslations } from "@openimis/fe-core";
+import { usePayersQuery } from "../hooks";
+
+const PayerPicker = (props) => {
+  const {
+    onChange,
+    readOnly = false,
+    required = false,
+    withLabel = true,
+    value,
+    label,
+    filterOptions,
+    filterSelectedOptions,
+    placeholder,
+    multiple = false,
+  } = props;
+  const modulesManager = useModulesManager();
+  const { formatMessage } = useTranslations("payer.PayerPicker", modulesManager);
+  const [searchString, setSearchString] = useState(null);
+  const { isLoading, data } = usePayersQuery({ filters: { name: searchString, first: 10 } }, { skip: true });
+
+  const options = data?.payers ?? [];
+
+  return (
+    <Autocomplete
+      multiple={multiple}
+      required={required}
+      placeholder={placeholder}
+      label={label ?? formatMessage("label")}
+      withLabel={withLabel}
+      readOnly={readOnly}
+      options={options}
+      isLoading={isLoading}
+      value={value}
+      getOptionLabel={(o) => o?.name}
+      onChange={onChange}
+      filterOptions={filterOptions}
+      filterSelectedOptions={filterSelectedOptions}
+      onInputChange={setSearchString}
+    />
+  );
+};
+
+export default PayerPicker;
